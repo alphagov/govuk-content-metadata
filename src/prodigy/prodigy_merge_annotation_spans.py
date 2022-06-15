d89bf1f7d5b88720cf66a3516deba321af93d676
rory-hurley-gds <rory.hurley@digital.cabinet-office.gov.uk>
2022-06-15 16:30:51 +0100

    update requirements.txt and src/prodigy... following PR comments.

--- src/prodigy/prodigy_merge_annotation_spans.py
+++ src/prodigy/prodigy_merge_annotation_spans.py
@@ -3,15 +3,16 @@
 from prodigy.components.db import connect
 from prodigy.models.ner import merge_spans
 
+output_dataset_name = 'merged_dataset_script'
 
 db = connect()  # connect to the DB using the prodigy.json settings
-datasets = ['sample_form_at_first80', 'sample_form_rh_correct', 'forms_rh_at_3000']
+datasets_to_merge = ['sample_form_at_first80', 'sample_form_rh_correct', 'forms_rh_at_3000']
 examples = []
-for dataset in datasets:
+for dataset in datasets_to_merge:
     examples += db.get_dataset(dataset)  # get examples from the database
 
-db.add_dataset('merged_dataset_script')
+db.add_dataset(output_dataset_name)
 
-db.add_examples(examples, datasets=['merged_dataset_script'])
+db.add_examples(examples, datasets=[output_dataset_name])
 
 
