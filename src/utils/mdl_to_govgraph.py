a3be32977390a51e52d5e31c1e82b8c5c1e77a27
rory-hurley-gds <rory.hurley@digital.cabinet-office.gov.uk>
2022-07-19 11:51:45 +0100

    functioning wrangling function

---
+++ src/utils/mdl_to_govgraph.py
@@ -0,0 +1,31 @@
+def mdl_to_govgraph(json_in):
+    """Wrangle a dictionary into format for ingestion into Neo4j format.
+
+    :param json_in: Input dictionary
+    :type json_in: dict
+    :return: Wrangled dictionary
+    :rtype: dict
+    """
+    base_path = json_in["base_path"]  # base path is base path
+    title_ents = [
+        (a, b) for (a, b, _, _) in json_in["title_ents"]
+    ]  # entity name and type from tuples only
+    description_ents = [(a, b) for (a, b, _, _) in json_in["description_ents"]]
+    text_ents = [(a, b) for (a, b, _, _) in json_in["text_ents"]]
+    setty = set(title_ents + description_ents + text_ents)  # get a set of unique tuples
+    entities = []  # instantiate list
+    for i in setty:
+        title_count, desc_count, text_count = (
+            title_ents.count(i),
+            description_ents.count(i),
+            text_ents.count(i),
+        )  # count occurences of tuple in each location
+        weight = [title_count, desc_count, text_count]  # create weight arra
+        entry = {
+            "entity_name": i[0],
+            "entity_type": i[1],
+            "weight": weight,
+        }  # convert entry to dict
+        entities.append(entry)  # append disct to list
+
+    return {"base_path": base_path, "entities": entities}  # return dictionary
