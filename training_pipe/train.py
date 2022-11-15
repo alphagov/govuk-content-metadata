e2512f944f748d4737de9541cf86e4600ce06139
rory-hurley-gds <rory.hurley@digital.cabinet-office.gov.uk>
2022-11-15 14:54:08 +0000

    gpu code updates

--- training_pipe/train.py
+++ training_pipe/train.py
@@ -1,9 +1,19 @@
-# from spacy.cli.train import train
-import json
+import spacy
+import thinc_gpu_ops
+from spacy.cli.train import train
 
-with open("data/training_set/phase_1_annotations.jsonl", "rb") as f:
-    result = [json.loads(jline) for jline in f.read().splitlines()]
+if __name__ == "__main__":
 
-# if __name__ == "__main__":
+    thinc_gpu_ops.AVAILABLE
 
-#     train("./config.cfg", overrides={"paths.train": "./train.spacy", "paths.dev": "./dev.spacy"})
+    spacy.require_gpu()
+
+    # train("./config.cfg", overrides={"paths.train": "./train.spacy", "paths.dev": "./dev.spacy"})
+    train(
+        config_path="phase_1_trf_config.cfg",
+        use_gpu=0,
+        overrides={
+            "paths.train": "phase_1_corpus/train.spacy",
+            "paths.dev": "phase_1_corpus/train.spacy",
+        },
+    )
