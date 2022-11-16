a1c34188a9e1f46a25e14ba995ef84a05517b1d2
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2022-11-16 15:31:37 +0000

    updated trainig_pipe files following working spacy-cuda docker image

--- training_pipe/train.py
+++ training_pipe/train.py
@@ -1,19 +1,21 @@
 import spacy
-import thinc_gpu_ops
 from spacy.cli.train import train
 
 if __name__ == "__main__":
 
-    thinc_gpu_ops.AVAILABLE
+    import time
 
-    spacy.require_gpu()
+    if spacy.require_gpu():
+        print("GPU good to go :D")
 
-    # train("./config.cfg", overrides={"paths.train": "./train.spacy", "paths.dev": "./dev.spacy"})
+    start = time.time()
     train(
         config_path="phase_1_trf_config.cfg",
         use_gpu=0,
         overrides={
             "paths.train": "phase_1_corpus/train.spacy",
-            "paths.dev": "phase_1_corpus/train.spacy",
+            "paths.dev": "phase_1_corpus/dev.spacy",
         },
     )
+    print(time.time() - start)
+    print("Training completed.")
