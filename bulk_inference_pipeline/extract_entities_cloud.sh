a2a0119a1a0d41dbd545d55bdc38a51ac214e4b2
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2022-11-18 21:42:09 +0000

    restructured bulk_inference_pipeline/extract_entities_cloud.sh
    now steps are grouped by part of page

--- bulk_inference_pipeline/extract_entities_cloud.sh
+++ bulk_inference_pipeline/extract_entities_cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# usage="Script to extract entities from pages on GOV.UK and stream output upload to Google Storage."
+# usage="Script to extract entities from pages on GOV.UK and load output upload to Google Storage."
 
 # The following options are available:
 #  -m    full path to NER model to use for inference.  (Required)
@@ -14,7 +14,7 @@
 # The script consists of 3 steps:
 # - Create input content data tables in Google Big Query
 # - Run NER (bulk) inferential pipeline
-# - Stream uploading of outcome files with extracted entities to Google Storage
+# - Upload outcome files with extracted entities to Google Storage
 # - Transfer the extracted entities files into corresponding BigQuery tables
 
 
@@ -52,23 +52,40 @@ echo "Creating input files in Google Big Query"
 python -m src.create_input_files
 echo "Input files created."
 
-echo "Running NER bulk inferential pipeline and streaming upload to Google Storage"
+echo "Starting NER bulk inferential pipeline and upload to Google Storage"
 
-echo "Starting NER bulk inferential pipeline for: 'title'"
-python -m src.extract_entities_cloud -p "title" -m ${NER_MODEL} -d ${TODAY} -b ${BATCH_SIZE} -n ${N_PROC}
-echo "Inference completed and data uploaded for: 'title'"
-
-echo "Starting NER bulk inferential pipeline for: 'description'"
-python -m src.extract_entities_cloud -p "description" -m ${NER_MODEL} -d ${TODAY} -b ${BATCH_SIZE} -n ${N_PROC}
-echo "Inference completed and data uploaded for: 'description'"
-
-echo "Starting NER bulk inferential pipeline for: 'text'"
-python -m src.extract_entities_cloud -p "text" -m ${NER_MODEL} -d ${TODAY}
-echo "Inference completed and data uploaded for: 'text'"
-
-
-echo "Export entities to Big Query"
+echo "Extracting entities from: TITLE"
+python -m extract_entities_cloud -p "title" -m ${NER_MODEL} -d ${TODAY} -b ${BATCH_SIZE} -n ${N_PROC}
+echo "Extraction completed."
+echo "Uploading file to Google Storage"
+gcloud storage cp entities_${TODAY}_title.jsonl gs://cpto-content-metadata/content_ner
+echo "Upload completed."
+echo "Exporting entities to Big Query"
 bq load --replace --source_format=NEWLINE_DELIMITED_JSON named_entities_raw.title gs://cpto-content-metadata/content_ner/entities_${TODAY}_title.jsonl entities_bq_schema
+echo "Entities exported."
+echo "Inference completed for: TITLE"
+rm -f entities_${TODAY}_title.jsonl
+
+echo "Extracting entities from: DESCRIPTION"
+python -m extract_entities_cloud -p "description" -m ${NER_MODEL} -d ${TODAY} -b ${BATCH_SIZE} -n ${N_PROC}
+echo "Extraction completed."
+echo "Uploading file to Google Storage"
+gcloud storage cp entities_${TODAY}_description.jsonl gs://cpto-content-metadata/content_ner
+echo "Upload completed."
+echo "Exporting entities to Big Query"
 bq load --replace --source_format=NEWLINE_DELIMITED_JSON named_entities_raw.description gs://cpto-content-metadata/content_ner/entities_${TODAY}_description.jsonl entities_bq_schema
+echo "Entities exported."
+echo "Inference completed for: DESCRIPTION"
+rm -f entities_${TODAY}_description.jsonl
+
+echo "Extracting entities from: TEXT"
+python -m extract_entities_cloud -p "text" -m ${NER_MODEL} -d ${TODAY} -b ${BATCH_SIZE} -n ${N_PROC}
+echo "Extraction completed."
+echo "Uploading file to Google Storage"
+gcloud storage cp entities_${TODAY}_text.jsonl gs://cpto-content-metadata/content_ner
+echo "Upload completed."
+echo "Exporting entities to Big Query"
 bq load --replace --source_format=NEWLINE_DELIMITED_JSON named_entities_raw.text gs://cpto-content-metadata/content_ner/entities_${TODAY}_text.jsonl entities_bq_schema
-echo "Entities exported successfully to Big Query"
+echo "Entities exported."
+echo "Inference completed for: TEXT"
+rm -f entities_${TODAY}_text.jsonl
