c3324a54f8944d28c3647bbf4276c6b418170415
Rory Hurley <rory.hurley@digital.cabinet-office.gov.uk>
2023-03-10 11:57:20 +0000

    update streamlit app to take outputs from 2 models and visualise spans

--- src/ner_streamlit_app/config.py
+++ src/ner_streamlit_app/config.py
@@ -1,18 +1,23 @@
 # this is a config file which controls some of the variables used in src.make_visualisations.visualise.py
 
 colors = {
+    "ORG": "#98fb98",
+    "PERSON": "#b0e0e6",
+    "GPE": "#ffdab9",
+    "TITLE": "#ffb6c1",
+    "ROLE": "#d8ac28",
+    "LOC": "#ccffff",
     "FORM": "#79dd6f",
-    "GPE": "#00ffcc",
     "MONEY": "#e4f60b",
-    "ORG": "#ff00ee",
-    "PERSON": "#ffdd00",
-    "DATE": "#fcf6d3",
+    "DATE": "#ffd700",
     "POSTCODE": "#f9b750",
-    "EMAIL": "#FADBEA",
+    "EMAIL": "#38ad04",
     "PHONE": "#aaff00",
+    "OCCUPATION": "#d0c2ef",
+    "FAC": "#f58a07",
+    "EVENT":"#0099ff"
 }
 
 entity_names = [k for (k, _) in colors.items()]
 
-# default_text = """Tom Read is CEO of GDS. He has been in the role since March 2021. GDS has a budget of £80 million for the 2021-2022 year. Whilst Mr Read has a background in MoJ, GDS has many employees who have worked for tech firms like Google and Apple, but also legal firms like Knockheart Mickleson. Prospective employees should get in touch with recruitment@digital.cabinet-office.gov.uk or call 0203 451 9000, they may ask to see your P45 or similar documents. Our office is located at 10 Whitechapel High Street, London E1 8QS."""  # noqa
-default_text = """Abisọla Fatokun is an enterprise digital transformation specialist with extensive public sector experience. He previously worked with the UK Government Digital Service’s Service Standards team ensuring that standards are met, and with the Ministry of Justice Digital & Technology team where he worked with senior leaders at the Legal Aid Agency, helping them to change to a service-led approach. Abisọla has also worked at KPMG, Accenture and BSkyB (now Sky). In 1999, Abisọla founded the first web development company in Nigeria, which he later sold."""  # noqa
+default_text = """Rishi Sunak became Prime Minister on 25 October 2022. He was previously appointed Chancellor of the Exchequer from 13 February 2020 to 5 July 2022. He was Chief Secretary to the Treasury from 24 July 2019 to 13 February 2020, and Parliamentary Under Secretary of State at the Ministry of Housing, Communities and Local Government from 9 January 2018 to 24 July 2019. Rishi went to Winchester College and studied Politics, Philosophy and Economics at Oxford University. He was also a Fulbright Scholar at Stanford University (USA) where he studied for his MBA. Rishi was elected Conservative MP for Richmond (Yorks) in May 2015 and served as a Parliamentary Private Secretary at the Department for Business, Energy and Industrial Strategy from June 2017 until his ministerial appointment."""  # noqa
