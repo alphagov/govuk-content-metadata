d978378251e7f8f7b9fec1105f6140694ea66466
rory-hurley-gds <91884393+rory-hurley-gds@users.noreply.github.com>
2023-03-13 09:23:55 +0000

    Update main.py
    Added docstrings to inform user about the data models and types.

--- src/fast_api_model_serving/main.py
+++ src/fast_api_model_serving/main.py
@@ -18,17 +18,29 @@ nlp = spacy.load("model-best")
 # Model classes
 # create a data model for requested url-content key-value pairs
 class Content(BaseModel):
+    """
+    post_url: The url of the line being fed into the model. Can be arbitrary
+                but good to have a reference to the page url.
+    content: The text to be fed into the model for inference.
+    """
     post_url: str
     content: str
 
 
 # create a data model for lists of Content model types
 class Payload(BaseModel):
+    """
+    A list of content, multiple {"post_url":"content"} value pairs.
+    """
     instances: List[Content]
 
 
 # create a data model for responses of text-entity key-value pairs
 class SingleEntity(BaseModel):
+    """
+    The data model that responses will be sent back by the server.
+    Returns text as well as the entities and their locations.
+    """
     text: str
     entity_type: str
     entity_startchar: str
@@ -37,6 +49,9 @@ class SingleEntity(BaseModel):
 
 # create a model for for lists of Single Entity key-value pairs
 class Entities(BaseModel):
+    """
+    A list of multiple responses from multiple requests payload.
+    """
     post_url: str
     entities: List[SingleEntity]
 
