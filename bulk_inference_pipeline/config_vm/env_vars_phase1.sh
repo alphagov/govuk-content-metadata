b3516a6a99dd62af18bbd46cb71d6d8ec51f2188
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2023-03-14 12:34:07 +0000

    make phase-1 bulk inference schedule 1h longer to play it safe

--- bulk_inference_pipeline/config_vm/env_vars_phase1.sh
+++ bulk_inference_pipeline/config_vm/env_vars_phase1.sh
@@ -3,4 +3,4 @@ DOCKER_IMAGE_NAME=ner-bulk-inference-phase1
 STARTUP_SCRIPT_PATH=config_vm/startup_script_vm_phase1.sh
 SCHEDULE_NAME=bulk-inference-phase1-schedule
 SCHEDULE_START_AT='10 0 1,15 * *'
-SCHEDULE_STOP_AT='0 21 1,15 * *'
+SCHEDULE_STOP_AT='0 22 1,15 * *'
