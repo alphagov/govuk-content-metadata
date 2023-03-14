d87b08da3d3d9b976d29fff7d3af2bf22b9a7649
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2023-03-14 12:20:50 +0000

    updated schedule for phase-2 bulk inference after decision to stagger it w.r.t. phase-1 inference to ensure GPU quota does not slow down execution

--- bulk_inference_pipeline/config_vm/env_vars_phase2.sh
+++ bulk_inference_pipeline/config_vm/env_vars_phase2.sh
@@ -2,5 +2,5 @@ VM_NAME=bulk-inference-phase2-ubuntu-gpu
 DOCKER_IMAGE_NAME=ner-bulk-inference-phase2
 STARTUP_SCRIPT_PATH=config_vm/startup_script_vm_phase2.sh
 SCHEDULE_NAME=bulk-inference-phase2-schedule
-SCHEDULE_START_AT='30 0 1,15 * *'
-SCHEDULE_STOP_AT='20 21 1,15 * *'
+SCHEDULE_START_AT='30 22 1,15 * *'
+SCHEDULE_STOP_AT='30 20 2,16 * *'
