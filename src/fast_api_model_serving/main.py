0046ab9243f23f7afd1dca750a7d1356472263db
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2023-03-27 11:56:49 +0100

    updated src/fast_api_model_serving/main.py to combine predictions from two spacy ner models; improved fast api functions and documentations

--- src/fast_api_model_serving/main.py
+++ src/fast_api_model_serving/main.py
@@ -1,102 +1,205 @@
 # This script is a FastAPI app which is intended to serve NER predictions from a model via HTTP calls.
-# For more information on the conventions used, please visit https://fastapi.tiangolo.com/tutorial/first-steps/
+# For more information on the conventions used, visit https://fastapi.tiangolo.com/tutorial/first-steps/
 # for comprehensive explainations.
 
-from fastapi import FastAPI
-from typing import List
-import spacy
-from pydantic import BaseModel
+# To run locally:
+# uvicorn main:app --reload
+# http://localhost:8000/docs
 
-# Initialisation
-# create a FastAPI instance
+
+from fastapi import FastAPI
+from typing import Union, Any
+import spacy
+from pydantic import BaseModel, HttpUrl, Field
+
+# Initialisation - create a FastAPI instance
 app = FastAPI()
 
-# load the spacy model
-nlp = spacy.load("model-best")
+print("Load the spacy models")
+nlp_phase1 = spacy.load("phase1_ner_trf_model/model-best")
+nlp_phase2 = spacy.load("phase2_ner_trf_model/model-best")
 
 
-# Model classes
-# create a data model for requested url-content key-value pairs
-class Content(BaseModel):
+def combine_ner_components(
+    ner_trf1: spacy.pipeline, ner_trf2: spacy.pipeline
+) -> spacy.pipeline:
     """
-    post_url: The url of the line being fed into the model. Can be arbitrary
-                but good to have a reference to the page url.
-    content: The text to be fed into the model for inference.
+    Takes in two spacy transformer pipeline objects, each containing a Named Entity Recognition (NER)
+    component, and returns a new spacy pipeline object that combines the two NER components.
+
+    Args:
+
+        ner_trf1: the first spacy transformer pipeline containing a NER component to be combined
+        ner_trf2: the second spacy transformer pipeline containing a NER component to be combined
+
+    Returns:
+        spacy.pipeline: a new spacy pipeline object that combines the two NER components,
+        placing the ner_trf2 NER component before the ner_trf1 NER component.
+
+    Ref: https://github.com/explosion/projects/tree/v3/tutorials/ner_double
+    """
+    # give this component a copy of its own tokenizer
+    ner_trf2.replace_listeners("transformer", "ner", ["model.tok2vec"])
+    # put the second ner component before the other ner
+    ner_trf1.add_pipe("ner", name="ner_2", source=ner_trf2, before="ner")
+    print(ner_trf1.pipe_names)
+    return ner_trf1
+
+
+nlp = combine_ner_components(nlp_phase1, nlp_phase2)
+
+# Request Bodies - Data Model class
+
+
+class InputContent(BaseModel):
+    """
+    Data model for the body prediction request.
     """
 
-    post_url: str
-    content: str
+    url: Union[HttpUrl, None] = Field(
+        default=None,
+        description="Valid URL of the webpage from which the text is taken.",
+    )
+    text: str = Field(
+        description="String of text to feed to the model to extract entities."
+    )
+    line_number: Union[int, None] = Field(
+        default=None, description="Line number of the text on the webpage"
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "url": "https://www.gov.uk/government/people/rishi-sunak",
+                "text": "Rishi Sunak became Prime Minister on 25 October 2022",
+                "line_number": 2,
+            }
+        }
 
 
-# create a data model for lists of Content model types
-class Payload(BaseModel):
+class InputContentVertexAI(BaseModel):
     """
-    A list of content, multiple {"post_url":"content"} value pairs.
+    JSON body format of prediction requests for Vertex AI.
+    Ref: https://cloud.google.com/vertex-ai/docs/predictions/custom-container-requirements
     """
 
-    instances: List[Content]
+    instances: list[InputContent]
 
 
-# create a data model for responses of text-entity key-value pairs
+# Response Data Models
+
+
 class SingleEntity(BaseModel):
     """
-    The data model that responses will be sent back by the server.
-    Returns text as well as the entities and their locations.
+    Basic response data model: returns the entities, their type and their locations.
     """
 
-    text: str
-    entity_type: str
-    entity_startchar: str
-    entity_endchar: str
+    name: str = Field(description="Name/text of the extracted entity instance.")
+    type: str = Field(
+        description="The entity type with which the entity name was tagged."
+    )
+    start: int = Field(
+        ge=0, description="The index of the first token of the entity name."
+    )
+    end: int = Field(description="The index of the last token of the entity name.")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "25 October 2022",
+                "type": "DATE",
+                "start": 37,
+                "end": 52,
+            }
+        }
 
 
-# create a model for for lists of Single Entity key-value pairs
-class Entities(BaseModel):
+class OutputEntities(BaseModel):
     """
-    A list of multiple responses from multiple requests payload.
+    Response data model: structure of responses that will be sent back by the server.
     """
 
-    post_url: str
-    entities: List[SingleEntity]
+    url: Union[HttpUrl, None] = Field(
+        default=None,
+        description="Valid URL of the webpage from which the text is taken.",
+    )
+    entities: list[SingleEntity]
+    line_number: Union[int, None] = Field(
+        default=None, description="Line number of the text on the webpage"
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "url": "https://www.gov.uk/government/people/rishi-sunak",
+                "entities": [
+                    {"name": "Rishi Sunak", "type": "PERSON", "start": 0, "end": 11},
+                    {"name": "Prime Minister", "type": "TITLE", "start": 19, "end": 33},
+                    {"name": "25 October 2022", "type": "DATE", "start": 37, "end": 52},
+                ],
+                "line_number": 2,
+            }
+        }
+
+
+class ResponseEntitiesVertexAI(BaseModel):
+    """
+    JSON body format of prediction response for Vertex AI.
+    Ref: https://cloud.google.com/vertex-ai/docs/predictions/custom-container-requirements
+    """
+
+    predictions: list[OutputEntities]
 
 
 # Request paths
-# get path for app root
+# GET endpoint for app root
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
 
 
-# get path for app health check to ensure server is running
+# GET endpoint for app health check to ensure server is running
 @app.get("/health_check", status_code=200)
 async def health_check():
     return {"response": "HTTP 200 OK"}
 
 
-# post path for getting ner predictions
-@app.post("/ner-service")
-async def get_ner(payload: Payload):
-    tokenize_content: List[spacy.tokens.doc.Doc] = [
-        nlp(content.content) for content in payload.instances
+# POST endpoints for predictions
+
+
+@app.post("/ner", response_model=OutputEntities)
+async def get_entities_one_doc(input: InputContent) -> Any:
+    document = nlp(input.text)
+    entities = [
+        {
+            "name": ent.text,
+            "type": ent.label_,
+            "start": ent.start_char,
+            "end": ent.end_char,
+        }
+        for ent in document.ents
     ]
-    document_enities = []
-    for doc in tokenize_content:
-        # GCP Vertex AI expects custom model payload to be a list of dicts [{'...':'...'}, {'...':'...'}, ...]
-        document_enities.append(
-            [
-                {
-                    "text": ent.text,
-                    "entity_type": ent.label_,
-                    "entity_startchar": ent.start_char,
-                    "entity_endchar": ent.end_char,
-                }
-                for ent in doc.ents
-            ]
-        )
-    # GCP Vertex AI expects custom models to return response {"predictions": [... , ...]}
+    return {"url": input.url, "entities": entities, "line_number": input.line_number}
+
+
+@app.post("/ner-vertex-ai", response_model=ResponseEntitiesVertexAI)
+async def get_entities(input: InputContentVertexAI) -> Any:
+    documents = [nlp(instance.text) for instance in input.instances]
+    entities = [
+        [
+            {
+                "name": ent.text,
+                "type": ent.label_,
+                "start": ent.start_char,
+                "end": ent.end_char,
+            }
+            for ent in document.ents
+        ]
+        for document in documents
+    ]
     return {
         "predictions": [
-            Entities(post_url=post.post_url, entities=ents)
-            for post, ents in zip(payload.instances, document_enities)
+            {"url": instance.url, "entities": ents, "line_number": instance.line_number}
+            for instance, ents in zip(input.instances, entities)
         ]
     }
