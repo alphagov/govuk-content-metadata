bba42143c0b55f2d07e47dc19ca414b353ebd47b
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2023-05-03 09:18:31 +0100

    saved sql queries for inference_pipeline_new_content from a .SQL to a .TXT extension so that they are easily compatible with google storage api called within Cloud Workflow

--- inference_pipeline_new_content/sql_queries/get_description.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  url,
-  description AS text
-FROM `govuk-knowledge-graph.content.description`
-INNER JOIN `cpto-content-metadata.content_ner_daily_new.{FILTERED_TABLE}` USING (url)
-;
--- inference_pipeline_new_content/sql_queries/get_title.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  url,
-  title AS text
-FROM `govuk-knowledge-graph.content.title`
-INNER JOIN `cpto-content-metadata.content_ner_daily_new.{FILTERED_TABLE}` USING (url)
-;
