6f3610e49096b6e91b068d87dfa369b010801a41
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2023-05-25 13:58:20 +0100

    updated src/post_extraction_process/deploy_post_extraction_gc_workflow.sh to log all call

--- src/post_extraction_process/deploy_post_extraction_gc_workflow.sh
+++ src/post_extraction_process/deploy_post_extraction_gc_workflow.sh
@@ -2,4 +2,5 @@ gcloud workflows deploy ${BULK_POSTPROC_WORKFLOW_NAME} \
     --source=${BULK_POSTPROC_WORKFLOW_YAML_PATH} \
     --location=${REGION} \
     --service-account=${NER_BULK_INFERENCE_SA} \
+    --call-log-level="log-all-calls" \
     --description="Bulk post-extraction processing and aggregation of named-entities in Big Query and export of counts table to Google Storage"
