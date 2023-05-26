7b2233c709f8c505f67ef1ea2185a10d95be3179
Alessia Tosi <alessia.tosi@digital.cabinet-office.gov.uk>
2023-05-26 16:00:55 +0100

    updated list of env variables for bulk inference phase-1 and phase-2

--- bulk_inference_pipeline/config_vm/env_vars_phase1.sh
+++ bulk_inference_pipeline/config_vm/env_vars_phase1.sh
@@ -1,6 +1,14 @@
-VM_NAME=bulk-inference-phase1-ubuntu-gpu
-DOCKER_IMAGE_NAME=ner-bulk-inference-phase1
-STARTUP_SCRIPT_PATH=config_vm/startup_script_vm_phase1.sh
-SCHEDULE_NAME=bulk-inference-phase1-schedule
-SCHEDULE_START_AT='10 0 1,15 * *'
-SCHEDULE_STOP_AT='0 22 1,15 * *'
+export N=1
+export VM_NAME=bulk-inference-phase1-ubuntu-gpu-cuda117
+export DOCKER_IMAGE_NAME=ner-bulk-inference-phase1
+export MACHINE_TYPE=n1-standard-16
+export ZONE=europe-west2-a
+export BOOT_DISK_SIZE=100
+export GPU_TYPE=nvidia-tesla-t4
+export GPU_COUNT=1
+export DESCRIPTION='govNER bulk inference phase-1 pipeline'
+# Google Scheduler
+export SCHEDULE_NAME=bulk-inference-phase1-schedule
+export SCHEDULE_DESCRIPTION='Once on the 1st of each month, start VMs at 0:10 AM and stop VMs at 22:00 PM'
+export SCHEDULE_START_AT='10 0 1 * *'
+export SCHEDULE_STOP_AT='0 22 1 * *'
